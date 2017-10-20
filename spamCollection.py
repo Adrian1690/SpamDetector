4cfb303da766f6c538ff2f7858d42647bd5fa883
Adrian Peña <j.adrian.1690@gmail.com>
2017-10-19 23:53:19 -0500

    Lesson 2 completed

--- spamCollection.py
+++ spamCollection.py
@@ -34,4 +34,49 @@ training_data = count_vector.fit_transform(X_train)
 # Transform testing data and return the matrix. Note we are not fitting the testing data into the CountVectorizer()
 testing_data = count_vector.transform(X_test)
 
-print(testing_data)
+#print(testing_data)
+
+'''
+Instructions:
+
+We have loaded the training data into the variable 'training_data' and the testing data into the 
+variable 'testing_data'.
+
+Import the MultinomialNB classifier and fit the training data into the classifier using fit(). Name your classifier
+'naive_bayes'. You will be training the classifier using 'training_data' and y_train' from our split earlier. 
+'''
+from sklearn.naive_bayes import MultinomialNB
+
+naive_bayes = MultinomialNB()
+naive_bayes.fit(training_data, y_train)
+
+'''
+Instructions:
+Now that our algorithm has been trained using the training data set we can now make some predictions on the test data
+stored in 'testing_data' using predict(). Save your predictions into the 'predictions' variable.
+'''
+
+predictions = naive_bayes.predict(testing_data)
+
+print(predictions)
+
+'''
+======================================
+        Evaluating our model
+======================================
+'''
+
+'''
+Instructions:
+Compute the accuracy, precision, recall and F1 scores of your model using your test data 'y_test' and the predictions
+you made earlier stored in the 'predictions' variable.
+'''
+
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+
+print('Accuracy score: ', format(accuracy_score(y_test, predictions)))
+print('Precision score: ', format(precision_score(y_test, predictions)))
+print('Recall score: ', format(recall_score(y_test, predictions)))
+print('F1 score: ', format(f1_score(y_test, predictions)))
+
+
